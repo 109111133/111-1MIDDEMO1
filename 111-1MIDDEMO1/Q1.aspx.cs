ac9d3446da726be784f2aa5faef8ea7a6bc42db0
109111133 <shelly901204@gmail.com>
2022-10-30 23:28:00 +0800

    1031

--- 111-1MIDDEMO1/Q1.aspx.cs
+++ 111-1MIDDEMO1/Q1.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace _111_1MIDDEMO1 {
     public partial class Q1 : System.Web.UI.Page {
@@ -11,6 +12,34 @@ namespace _111_1MIDDEMO1 {
 
         protected void Page_Load(object sender, EventArgs e) {
 
+        }
+
+        protected void tb_Account_TextChanged(object sender, EventArgs e)
+        {
+            string shelly=tb_Account.Text;
+            foreach (string s in s_IdSet)
+            {
+                if (s ==shelly)
+                {
+                    lb_Type.Text = "複診";
+                    break;
+                }
+                else lb_Type.Text = "初診";
+
+            }
+            if (shelly=="") btn_Submit.Visible=false;
+            else btn_Submit.Visible=true;
+            pl_Msg.Visible=false;
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            pl_Msg.Visible=true;
+            lb_Msg.Text =lb_Type.Text+"<br />"+tb_Account.Text+"先生小姐，已完成掛號。"+"<br />";
+            if (tb_Email.Text!="") 
+            {
+                lb_Msg.Text +="已寄信至信箱"+tb_Email.Text+"<br />";
+            }
         }
     }
 }
